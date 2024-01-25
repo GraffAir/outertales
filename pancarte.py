2a4bbf9b0e2a27f00a738abea596bb62d85e5e17
RafffGI <rafaelcornitte05@gmail.com>
2024-01-25 09:29:49 +0100

    pancartes
    j'ai créé le fichier pancarte

---
+++ pancarte.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Pancarte:
+    def __init__(self, image, coordonnées_images):
+        self.image = image
+        self.coordonnées = coordonnées_images
+
+    def ouvrir_image(self):
