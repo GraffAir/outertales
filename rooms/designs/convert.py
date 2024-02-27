0a480680fcf4264996872d9f0a36ce91799fcba5
UnHommeMysterieux <136302599+UnHommeMysterieux@users.noreply.github.com>
2024-02-27 16:16:17 +0100

    Delete rooms directory

--- rooms/designs/convert.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import room1, room2, room3, room4
-
-num = room4.num
-obj = room4.obj
-
-with open(f"rooms/room{num}.bin", "wb") as pickle_in:
-    pickle.dump(obj, pickle_in, pickle.HIGHEST_PROTOCOL)
