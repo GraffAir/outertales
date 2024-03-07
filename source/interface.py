a990d52394e11925c142798a9360c49ff97317d4
GraffAir <123460802+GraffAir@users.noreply.github.com>
2024-03-07 19:56:51 +0100

    Mise à jour du menu

--- source/interface.py
+++ source/interface.py
@@ -1,8 +1,20 @@
 import pygame
+pygame.font.init()
+
+font_header1 = pygame.font.SysFont('Courier New', 70)
+font_header2 = pygame.font.SysFont('Courier New', 50)
+
+# on définit les images du menu
+play_btn_img = pygame.image.load("images/menu/play_btn.jpg")
+menu_bg_img = pygame.transform.scale(pygame.image.load("images/menu/menu_bg.png"), (1280, 720))
+
+# les textes
+text_title = font_header1.render("Outertale", False, (255, 255, 255))
+textbutton_play = font_header2.render("Jouer", False, (255, 255, 255))
 
 class Button:
-    def __init__(self, x, y, img):
-        self.image = pygame.transform.scale(img, (40 * 6, 40 * 2))
+    def __init__(self, x, y, img, size):
+        self.image = pygame.transform.scale(img, size)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -25,16 +37,13 @@ class Button:
         screen.blit(self.image, self.rect)
 
         return action
-    
-# on définit
-# les images du menu
-play_btn_img = pygame.image.load("images/menu/play_btn.jpg")
-menu_bg_img = pygame.transform.scale(pygame.image.load("images/menu/menu_bg.png"), (1280, 720))
 
-# les bouttons
-play_btn = Button(50, 50, play_btn_img)
+# on définit les bouttons
+play_btn = Button(100, 170, textbutton_play, (30 * 6, 30 * 2))
 
 def draw_menu(screen):
+    """fonction qui sera appellé à chaque boucle du jeu où le menu est allumé"""
     screen.blit(menu_bg_img, (0, 0))
+    screen.blit(text_title, (100, 100))
     return play_btn.draw(screen)
             
