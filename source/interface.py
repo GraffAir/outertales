33804dcb5ece2051dd25c75011e820d10dbfd7dd
GraffAir <123460802+GraffAir@users.noreply.github.com>
2024-03-16 14:51:58 +0100

    Menu principal (bouton quitter, fonctions internes, début menu paramètres)

--- source/interface.py
+++ source/interface.py
@@ -3,12 +3,16 @@ pygame.font.init()
 
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+ACCENT_COLOR = (53, 144, 247)
 
-font_header1 = pygame.font.Font('fonts/LTSoul-Bold.otf', 70)
-font_header2 = pygame.font.SysFont('Courier New', 50)
+font_header1 = pygame.font.Font('fonts/LTSoul-Bold.otf', 90)
+font_header2 = pygame.font.SysFont('LTSoul-Regular', 50)
+font_button = pygame.font.SysFont('LTSoul-Regular', 50)
 
 # on définit les images du menu
 menu_bg_img = pygame.transform.scale(pygame.image.load("images/menu/menu_bg.png"), (SCREEN_WIDTH, SCREEN_HEIGHT))
+settings_bg_img = pygame.Surface(menu_bg_img.get_size(), 32)
+settings_bg_img.set_alpha(200, pygame.RLEACCEL)
 
 # les textes
 text_title = font_header1.render("Outertale", False, (255, 255, 255))
@@ -16,14 +20,25 @@ textbutton_play = font_header2.render("Jouer", False, (255, 255, 255))
 textbutton_settings = font_header2.render("Paramètres", False, (255, 255, 255))
 textbutton_leave = font_header2.render("Quitter", False, (255, 255, 255))
 
-class Button:
-    def __init__(self, relative_pos, img, size):
-        relx, rely = relative_pos
-        width, height = size
-        #1280*relx - width/2
 
-        absx = SCREEN_WIDTH * relx - width/2
-        absy = SCREEN_HEIGHT * rely - height/2
+# les textes du menu paramètres
+textbutton_audio = font_button.render("Audio", False, ACCENT_COLOR)
+textbutton_controles = font_button.render("Contrôles", False, ACCENT_COLOR)
+
+def get_absolute(position, size, anchorpoint=(0, 0)):
+    relx, rely = position
+    relwidth, relheight = size
+    width, height = (SCREEN_WIDTH * relwidth), (SCREEN_HEIGHT * relheight)
+    absx = SCREEN_WIDTH * relx - width*anchorpoint[0]
+    absy = SCREEN_HEIGHT * rely - height*anchorpoint[1]
+    assert width < 10000 and height < 10000 and absx <= SCREEN_WIDTH and absy <= SCREEN_HEIGHT, "Absolute Value Out of Range"
+    return (absx, absy), (width, height)
+
+class Button:
+    def __init__(self, pos, img, rel_size, anchorpoint=(0,0)):
+
+        abs_pos, size = get_absolute(pos, rel_size, anchorpoint)
+        absx, absy = abs_pos
 
         self.image = pygame.transform.scale(img, size)
         self.hoverimage = pygame.transform.scale(img, (size[0]*1.2, size[1]*1.2))
@@ -61,17 +76,54 @@ class Button:
 
 # on définit les bouttons
     
-play_btn = Button((0.5, 0.5), textbutton_play, (30 * 6, 30 * 2))
-settings_btn = Button((0.25, 0.5), textbutton_settings, (30 * 5, 30))
-leave_btn = Button((0.75, 0.5), textbutton_leave, (30 * 4, 30))
+play_btn = Button((0.4, 0.5), textbutton_play, (0.15, 0.07), (0, 1))
+settings_btn = Button((0.18, 0.5), textbutton_settings, (0.13, 0.05), (0, 1))
+leave_btn = Button((0.65, 0.5), textbutton_leave, (0.12, 0.05), (0, 1))
+    
 
 
+class Menu:
+    def __init__(self, screen):
+        self.current_window = "menu" # menu / settings / pause
+        self.screen = screen
+        black_img = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+        self.black_img = black_img.convert_alpha()
 
-def draw_menu(screen):
-    """fonction qui sera appellé à chaque boucle du jeu où le menu est allumé"""
-    screen.blit(menu_bg_img, (0, 0))
-    screen.blit(text_title, (490, 100))
-    settings_btn.draw(screen)
-    leave_btn.draw(screen)
-    return play_btn.draw(screen)
+    def draw(self):
+        """fonction qui sera appellé à chaque boucle du jeu où le menu est allumé"""
+        pass
+        if self.current_window == "menu":
+            self.screen.blit(menu_bg_img, (0, 0))
+            self.screen.blit(text_title, (450, 100))
+            if settings_btn.draw(self.screen):
+                self.current_window = "settings"
+
+            if leave_btn.draw(self.screen):
+                pygame.quit()
             
+            return play_btn.draw(self.screen)
+
+        elif self.current_window == "settings":
+            self.screen.blit(menu_bg_img, (0, 0))
+            self.screen.blit(settings_bg_img, (0, 0))
+
+            def textButton(x, y, width, height, surface):
+                (absx, absy), (abswidth, absheight) = get_absolute((x, y), (width, height))
+                pygame.draw.rect(self.screen, (53, 144, 247), pygame.Rect(absx, absy, abswidth, absheight), 3)
+                
+                self.screen.blit(surface, (absx+abswidth/2-surface.get_width()/2, absy+absheight/2-surface.get_height()/2)) 
+                
+            
+            textButton(0.3, 0.2, 0.2, 0.1, textbutton_audio)
+            textButton(0.5, 0.2, 0.2, 0.1, textbutton_controles)
+
+
+
+
+
+            return False
+        
+        
+
+        
+                
